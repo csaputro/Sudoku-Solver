13e7de4b0f409bd77f2321abf6f345c65d7cdcbb
Christian Saputro <tui01777@temple.edu>
2022-03-31 00:12:15 -0400

    created helper methods in boolean in case if a number exist on the board.

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -1,4 +1,7 @@
 public class SudokuSolver {
+    //declares static gridsize to use for later
+    private static final int GridSize = 9;
+
     public static void main(String[] args) {
 
         //declares 2D array to hold the board
@@ -13,5 +16,16 @@ public class SudokuSolver {
                 {0, 0, 0, 2, 0, 0, 0, 0, 0},
                 {0, 0, 7, 0, 4, 0, 2, 0, 3},
         };
+    }
+
+    //helper methods boolean if theres number true, else false
+    private static boolean numInRow(int[][] board, int number, int row){
+
+        for(int i = 0; i < GridSize; i++){
+            if(board[row][i] == number){
+                return true;
+            }
+        }
+        return false;
     }
 }
